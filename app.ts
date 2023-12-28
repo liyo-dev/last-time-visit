ff9677bad1a23c2084a32ca04245dbaabf29bb99
liyodev <luarbaz@gmail.com>
2023-12-28 21:13:17 +0100

    cors 3

--- app.ts
+++ app.ts
@@ -1,21 +1,17 @@
 /// <reference lib="deno.unstable" />
 import { Hono } from "https://deno.land/x/hono@v3.11.10/mod.ts";
-import { serveStatic } from "https://deno.land/x/hono@v3.11.10/middleware.ts";
+import { cors, serveStatic } from "https://deno.land/x/hono@v3.11.10/middleware.ts";
 import { streamSSE } from "https://deno.land/x/hono@v3.11.10/helper/streaming/index.ts";
 
 const db = await Deno.openKv();
 const app = new Hono();
 
-// Configuración de encabezados CORS
-app.use(async (c) => {
-    c.res.headers.set("Access-Control-Allow-Origin", "https://bitter-bison-62-yr772rrzbxdt.deno.dev");
-    c.res.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    c.res.headers.set("Access-Control-Allow-Methods", "OPTIONS, POST, GET");
-});
-
 let i = 0;
 
+app.use(cors())
+
 app.get('/', serveStatic({ path: './index.html' }));
+
 
 app.post('/visit', async(c)=>{
     const { country, city, flag } = await c.req.json()
@@ -24,19 +20,38 @@ app.post('/visit', async(c)=>{
     .sum(["visits"], 1n)
     .commit()
     return c.json({message: 'ok'})
-});
+})
 
 app.get('/visit', (c) => {
     return streamSSE(c, async (stream) => {
-        const watcher = db.watch([["lastVisit"]])
+      const watcher = db.watch([["lastVisit"]])
   
-        for await (const entry of watcher){
-            const { value } = entry[0]
-            if (value != null){
-                await stream.writeSSE({ data:JSON.stringify(value), event: 'update', id: String(i++) })
-            }
-        }
-    });
+      for await (const entry of watcher){
+          const { value } = entry[0]
+          if (value != null){
+              await stream.writeSSE({ data:JSON.stringify(value), event: 'update', id: String(i++) })
+          }
+      }
+
+// app.get('/counter', (c) => {
+//   return streamSSE(c, async (stream) => {
+//     const watcher = db.watch([["visits"]])
+
+//     for await (const entry of watcher){
+//         const { value } = entry[0]
+//         if (value != null){
+//             await stream.writeSSE({ data: Number(value).toString(), event: 'update', id: String(i++) })
+//         }
+//     }
+
+    // while(true){
+    //     const { value } = await db.get(["visits"])
+    //     await stream.writeSSE({ data: Number(value).toString(), event: 'update', id: String(i++) })
+    //    const message = `Son las ${new Date().toLocaleTimeString()}`
+    //    await stream.writeSSE({ data: message, event: 'update', id: String(i++) })
+    //     await stream.sleep(1000)
+    // }
+  })
 });
 
 Deno.serve(app.fetch);
